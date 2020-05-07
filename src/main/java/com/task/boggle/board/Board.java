bb91ef3cdf1358929aca046eaa81622422ca6e4a
Subedia <arjun.subedi@threemonk.com>
2020-05-07 23:50:08 +0545

    add puzzel veriable in board entity to get list of 4*4 charecter board

--- src/main/java/com/task/boggle/board/Board.java
+++ src/main/java/com/task/boggle/board/Board.java
@@ -1,4 +1,13 @@
 package com.task.boggle.board;
 
 public class Board {
+    private char[][] puzzel;
+
+    public char[][] getPuzzel() {
+        return puzzel;
+    }
+
+    public void setPuzzel(char[][] puzzel) {
+        this.puzzel = puzzel;
+    }
 }
