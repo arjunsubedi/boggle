772364ea437933ec1ef253505b1fc0d92be570dc
Subedia <arjun.subedi@threemonk.com>
2020-05-07 07:09:36 +0545

    the dictionary file is adding

--- src/main/java/com/task/buggle/BuggleApplication.java
+++ src/main/java/com/task/buggle/BuggleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BuggleApplication {
 
+    private static final String FILE_NAME="words_alpha.txt";
     public static void main(String[] args) {
         SpringApplication.run(BuggleApplication.class, args);
     }
