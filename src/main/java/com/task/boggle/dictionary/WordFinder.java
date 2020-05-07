e1e46e5ad0b95b78555b840648a99a9bdb65e2ef
Subedia <arjun.subedi@threemonk.com>
2020-05-07 19:58:15 +0545

    store word by its index

--- src/main/java/com/task/boggle/dictionary/WordFinder.java
+++ src/main/java/com/task/boggle/dictionary/WordFinder.java
@@ -4,7 +4,23 @@ import java.util.List;
 
 public class WordFinder {
 
+    private static DictionaryWord root;
     public void setWordList(List<String> wordList) {
+        root = new DictionaryWord();
+        for(String word: wordList) insert(root,word);
+    }
+
+    private void insert(DictionaryWord root, String Key) {
+        int n = Key.length();
+        DictionaryWord pChild = root;
+        for(int i=0; i<n; i++){
+            int index = Key.charAt(i) - 'A';
+            if(pChild.Child == null){
+                pChild.Child[index] = new DictionaryWord();
+            pChild =pChild.Child[index];
+            }
+            pChild.end = true;
+        }
 
     }
 }
