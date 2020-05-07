1b797796bb67f7197ef07d04876dac0b24f5aafb
Subedia <arjun.subedi@threemonk.com>
2020-05-07 06:45:51 +0545

    the initital commit

---
+++ src/main/java/com/task/buggle/BuggleApplication.java
@@ -0,0 +1,13 @@
+package com.task.buggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuggleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuggleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/buggle/BuggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.buggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuggleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
