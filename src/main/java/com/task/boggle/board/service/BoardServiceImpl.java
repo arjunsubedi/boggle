d3435402e758c6a57c8c6f860922ea009635b450
Subedia <arjun.subedi@threemonk.com>
2020-05-08 08:06:16 +0545

    add method for getting board puzzel

--- src/main/java/com/task/boggle/board/service/BoardServiceImpl.java
+++ src/main/java/com/task/boggle/board/service/BoardServiceImpl.java
@@ -1,4 +1,16 @@
 package com.task.boggle.board.service;
 
-public class BoardServiceImpl {
+import com.task.boggle.board.Board;
+
+public class BoardServiceImpl implements BoardService{
+    @Override
+    public Board getRandomBoard() {
+        Board board = new Board();
+        board.setPuzzel(getPuzzel());
+        return board;
+    }
+
+    private char[][] getPuzzel() {
+
+    }
 }
